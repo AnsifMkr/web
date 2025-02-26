9d0cdc15cfd0acb96079433bbd6e97de3cb0d333
Ansif <99172928+AnsifMkr@users.noreply.github.com>
2025-02-27 00:38:13 +0530

    Update tailwind.config.js

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -3,9 +3,7 @@ export default {
   content: ["./index.html",
     "./src/**/*.{js,ts,jsx,tsx}",],
   theme: {
-    extend: {backgroundImage: {
-      'hero': "url('./src/assets/hospital-photo.png')",
-    },},
+    extend: {},
   },
   plugins: [],
 }
