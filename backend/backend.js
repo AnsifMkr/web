bb16afda19c3b93cd540f864eabc62d4e1eff4d1
Pratheek Rao K B <pratheekmulleria@gmail.com>
2025-03-05 19:13:01 +0530

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -199,6 +199,8 @@ app.get('/', (req, res) => {
   res.send('API is working');
 });
 
-app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
-});
+// app.listen(PORT, () => {
+//   console.log(`Server running on http://localhost:${PORT}`);
+// });
+
+module.exports = app;
