3100c13fa555041bb7429d8faa787959c64bd4c4
Pratheek Rao K B <pratheekmulleria@gmail.com>
2025-03-05 19:09:53 +0530

    Update backend.js

--- backend/backend.js
+++ backend/backend.js
@@ -8,18 +8,7 @@ const app = express();
 app.use(express.json());
 
 
-
-app.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', 'https://web-frontend-jet.vercel.app');
-  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
-  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-  res.header('Access-Control-Allow-Credentials', 'true');
-
-  if (req.method === 'OPTIONS') {
-    return res.status(200).send();
-  }
-  next();
-});
+app.use(cors());
 
 // ✅ Handle preflight requests properly
 app.options('*', cors());
